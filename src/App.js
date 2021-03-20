bfc1621830ceec175c190a501fa941cd0231c99e
Joaquin Salas <joaquinsalas-1993@hotmail.com>
2021-03-20 11:31:38 -0600

    Primer commit
    Se eliminaron los archivos que trae por defecto el proyecto React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+/*imr y tab para importar */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "container mt-5 text-center" >
+        <
+        h1 > Tareas < /h1> <
+        hr className = "mt-2" / >
+        <
+        /div>
+    );
 }
 
 export default App;
