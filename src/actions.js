c0d0eb1ce0c690dff5ae452564fc8d10eee70111
Joaquin Salas <joaquinsalas-1993@hotmail.com>
2021-03-23 11:10:30 -0600

    Update 23-32021 11:10 am

--- src/actions.js
+++ src/actions.js
@@ -4,20 +4,61 @@ import 'firebase/firestore'
 
 const db = firebase.firestore(firebaseApp)
 
-/*el parametro collection, es el nombre de la coleccion que se creo en firebase (tasks)*/
 export const getCollection = async(collection) => {
     const result = { statusResponse: false, data: null, error: null }
-
     try {
         const data = await db.collection(collection).get()
         const arrayData = data.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-        result.statusResponse = true;
-        result.data = arrayData;
-        console.log(arrayData);
+        result.statusResponse = true
+        result.data = arrayData
     } catch (error) {
-        result.error = error;
+        result.error = error
     }
+    return result
+}
 
-    return result;
+export const addDocument = async(collection, data) => {
+    const result = { statusResponse: false, data: null, error: null }
+    try {
+        const response = await db.collection(collection).add(data)
+        result.data = { id: response.id }
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+    return result
+}
 
+export const getDocument = async(collection, id) => {
+    const result = { statusResponse: false, data: null, error: null }
+    try {
+        const response = await db.collection(collection).doc(id).get()
+        result.data = { id: response.id, ...response.data() }
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+    return result
+}
+
+export const updateDocument = async(collection, id, data) => {
+    const result = { statusResponse: false, error: null }
+    try {
+        await db.collection(collection).doc(id).update(data)
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+    return result
+}
+
+export const deleteDocument = async(collection, id) => {
+    const result = { statusResponse: false, error: null }
+    try {
+        await db.collection(collection).doc(id).delete()
+        result.statusResponse = true
+    } catch (error) {
+        result.error = error
+    }
+    return result
 }
